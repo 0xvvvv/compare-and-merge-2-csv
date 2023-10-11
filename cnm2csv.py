6bcb52c156030dff72e2067c83032e0b45f00b95
O V <116284794+0xvvvv@users.noreply.github.com>
2023-10-11 23:26:16 +0600

    Create cnm2csv.py

---
+++ cnm2csv.py
@@ -0,0 +1,25 @@
+import csv
+
+def merge_csv(input_file1, input_file2, output_file):
+    # Чтение второго CSV файла и создание словаря с парами (слово, значение)
+    with open(input_file2, 'r', newline='', encoding='utf-8') as file2:
+        reader2 = csv.reader(file2)
+        word_value_dict = {row[0]: row[1] for row in reader2}
+
+    # Обработка первого CSV файла
+    with open(input_file1, 'r', newline='', encoding='utf-8') as file1:
+        reader1 = csv.reader(file1)
+        data = []
+        for row in reader1:
+            word = row[0]
+            value = word_value_dict.get(word, '')
+            row.append(value)
+            data.append(row)
+
+    # Запись результата в новый CSV файл
+    with open(output_file, 'w', newline='', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerows(data)
+
+# запускаем функцию
+merge_csv('input1.csv', 'input2.csv', 'output.csv')
